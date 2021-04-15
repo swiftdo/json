1f00d6772b01cc37904ac6d9af58a2872684a3bd
lai <1164258202@qq.com>
2021-04-15 14:32:51 +0800

    Feat: json 2.0

--- json/Parser2.swift
+++ json/Parser2.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+struct JsonParserError: Error {
+    var msg: String
+    init(msg: String) {
+        self.msg = msg
+    }
+}
+
 /// 分词 + AST
 // [自己动手实现一个简单的JSON解析器](https://segmentfault.com/a/1190000010998941)
 
@@ -64,18 +71,104 @@ struct JsonTokenizer {
         currentIndex = input.index(after: currentIndex)
     }
     
-    mutating func nextToken() -> JsonToken? {
-        guard let c = current else {
+    mutating func nextToken() throws -> JsonToken? {
+        guard let _ = current else {
+            return nil
+        }
+        scanSpaces()
+        
+        guard let ch = current else {
             return nil
         }
         
-        var token: JsonToken?
-        
-        // todo
-        
-        
+        switch ch {
+        case "{":
+            return JsonToken.objBegin
+        case "}":
+            return JsonToken.objEnd
+        case "[":
+            return JsonToken.arrBegin
+        case "]":
+            return JsonToken.arrEnd
+        case ",":
+            return JsonToken.sepComma
+        case ":":
+            return JsonToken.sepColon
+        case "n":
+            return try JsonToken.null(scanMatch(string: "null"))
+        case "t":
+            return try JsonToken.bool(scanMatch(string: "true"))
+        case "f":
+            return try JsonToken.bool(scanMatch(string: "false"))
+        case "\"":
+            return nil
+        case _ where isNumber(c: ch):
+            return try JsonToken.number(scanNumbers())
+        default:
+              throw JsonParserError(msg: "无法解析的字符:\(ch)")
+        }
     }
     
+    mutating func scanNumbers() throws -> String {
+        let ind = currentIndex
+        while let c = current, isNumber(c: c) {
+            advance()
+        }
+        if currentIndex != ind {
+            return String(input[ind..<currentIndex])
+        }
+        throw JsonParserError(msg: "scanNumbers 出错:\(ind)")
+    }
+    
+    mutating func scanMatch(string: String) throws -> String {
+        return try scanMatch(characters: string.map { $0 })
+    }
+    
+    mutating func scanSpaces() {
+        while current != " " {
+            advance()
+        }
+    }
+    
+    mutating func scanMatch(characters: [Character]) throws -> String {
+        let ind = currentIndex
+        var isMatch = true
+        for index in (0..<characters.count) {
+            if characters[index] != current {
+                isMatch = false
+                break
+            }
+            advance()
+        }
+        if (isMatch) {
+            return String(input[ind..<currentIndex])
+        }
+        throw JsonParserError(msg: "scanUntil 不满足 \(characters)")
+    }
+    
+   
+    /// 判断是否是数字字符
+    func isNumber(c: Character) -> Bool {
+        
+        let chars:[Character: Bool] = ["-": true, "+": true, "e": true, "E": true, ".": true]
+        
+        if let b = chars[c], b {
+            return true
+        }
+        
+        if(c >= "0" && c <= "9") {
+            return true
+        }
+        
+        return false;
+    }
+
+    /// 判断是否为 16 进制字符
+    func isHex(c: Character) -> Bool {
+        return c >= "0" && c <= "9" || c >= "a" && c <= "f" || c >= "A" && c <= "F"
+    }
+
 }
+
 
 
